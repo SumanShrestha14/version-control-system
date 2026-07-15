8795a2926269b78152852cb951c72b4c9476686a
Suman Shrestha <sumanstha0614@gmail.com>
2026-07-15 23:02:29 +0545

    02:: Fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
+#include <cstdlib>
 #include <iostream>
 #include <string>
 #include "utils/command.h"
 
 using namespace std;
 
-string decompress(const string &compressed);
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     cerr << "No command provided.\n";
@@ -14,7 +14,13 @@ int main(int argc, char *argv[]) {
   string command = argv[1];
 
   if (command == "init") {
-    git_init();
+      if(git_init()){
+          cerr << "Successfully initialized repository.\n";
+          return EXIT_SUCCESS;
+      }else{
+          cerr << "Failed to initialize repository.\n";
+          return EXIT_FAILURE;
+      }
   } else if (command == "cat-file") {
     if (argc != 4) {
       cerr << "Usage: git cat-file -p <sha1>\n";
@@ -22,7 +28,13 @@ int main(int argc, char *argv[]) {
     }
     string flag = argv[2];
     string sha1 = argv[3];
-    cat_file(sha1, flag);
+    if(cat_file(sha1, flag)){
+        cout<<"Successfully retrieved blob content.\n";
+        return EXIT_SUCCESS;
+    }else{
+        cerr << "Failed to retrieve blob content.\n";
+        return EXIT_FAILURE;
+    }
   } else {
     cerr << "Unknown command " << command << '\n';
     return EXIT_FAILURE;
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -2,12 +2,6 @@
 #define UTILS_H
 
 #include <string>
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <zlib.h>
-#include <stdexcept>
-
 std::string decompress(const std::string& compressedData);
 
 #endif
